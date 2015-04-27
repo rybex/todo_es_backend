bfcca4b49dc44dc202cf37a8475647917a485f87
rybex <tomek.rybka@gmail.com>
2015-04-27 22:14:08 +0200

    Added few errors

--- app/domain/common/errors.rb
+++ app/domain/common/errors.rb
@@ -1,3 +1,6 @@
 class Errors
   IncorrectAttributes = Class.new(StandardError)
+  AlreadyCreated      = Class.new(StandardError)
+  AlreadyRemoved      = Class.new(StandardError)
+  ItemNotExist        = Class.new(StandardError)
 end
